2935bcae4d18185711259a8d5cd38930f6e43f15
Bloem <semhammann@gmail.com>
2025-03-17 13:46:40 +0100

    added some new packages

--- init.lua
+++ init.lua
@@ -1,13 +1,2 @@
-if vim.g.vscode then
-   -- VSCode extension
-    -- these ones don't work
-    vim.keymap.set('n', '<C-u>', '<C-u>zz')
-    vim.keymap.set('n', '<C-d>', '<C-d>zz') 
-else
-   -- ordinary Neovim
-    vim.keymap.set('n', '<C-u>', '<C-u>zz')
-    vim.keymap.set('n', '<C-d>', '<C-d>zz') 
-end
-
---vim.keymap.set('n', '<C-u>', '<C-u>zz')
---vim.keymap.set('n', '<C-d>', '<C-d>zz') 
+require("config.lazy")
+require("bloem")
