2ca1a3757a69816f799bab7694595bd300bbd039
Bloem <semhammann@gmail.com>
2025-03-26 20:11:32 +0100

    updated remap en treesitter

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -3,9 +3,10 @@ local configs = require("nvim-treesitter.configs")
 configs.setup({
     ensure_installed = { "c", "cpp", "python", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
     sync_install = false,
+    auto_install = true,
     highlight = {
         enable = true,
-    additional_vim_regex_highlighting = false,
+        additional_vim_regex_highlighting = false,
     },
     indent = { enable = true },  
 })
--- lua/bloem/remap.lua
+++ lua/bloem/remap.lua
@@ -8,4 +8,7 @@ vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', 'n', 'nzz')
 vim.keymap.set('n', 'N', 'Nzz')
 
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
 vim.keymap.set('x', '<leader>p', '\"_dP')
