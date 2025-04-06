93ceaf60ff129dd785283d7378fa9bd6097665c1
Bloem <semhammann@gmail.com>
2025-04-06 19:47:31 +0200

    updated set.lua

--- lua/bloem/set.lua
+++ lua/bloem/set.lua
@@ -15,6 +15,11 @@ vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
 
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
