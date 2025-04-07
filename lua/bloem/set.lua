2d19561399e8a63b95e5cce1382c945f2f91ccd7
Bloem <sem@ie-design.nl>
2025-04-07 15:57:02 +0200

    now there shouldn't be an error on windows

--- lua/bloem/set.lua
+++ lua/bloem/set.lua
@@ -17,7 +17,7 @@ vim.opt.termguicolors = true
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undodir = (os.getenv("HOME") or os.getenv("USERPROFILE")) .. "/.vim/undodir"
 vim.opt.undofile = true
 
 vim.opt.scrolloff = 8
