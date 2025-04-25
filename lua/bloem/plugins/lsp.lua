830c0a5925ad24be7ecef928aa2c8ffa27faf8e4
Bloem <semhammann@gmail.com>
2025-04-25 12:42:38 +0200

    update lsp config

--- lua/bloem/plugins/lsp.lua
+++ lua/bloem/plugins/lsp.lua
@@ -3,13 +3,14 @@ return{
         'neovim/nvim-lspconfig',
 
         dependencies = {
-        "williamboman/mason-lspconfig.nvim",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-nvim-lua",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-cmdline",
-        "hrsh7th/nvim-cmp",
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-nvim-lua",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-cmdline",
+            "hrsh7th/nvim-cmp",
         },
 
         config = function()
@@ -18,36 +19,48 @@ return{
                 automatic_installation = true,
             }
 
-            local lspconfig = require("lspconfig")
 
-            local servers = { "lua_ls", "clangd", "pyright" }
 
-            for _, server in ipairs(servers) do
-                local opts = {}
 
-                -- add Factorio-specific settings for lua_ls
-                if server == "lua_ls" then
-                    -- if on Windows
-                    if os.getenv("HOME") == nil then
-                    opts.on_new_config = function(new_config, new_root_dir)
-                        if new_root_dir:find("Factorio") then
-                                new_config.settings = vim.tbl_deep_extend("force", new_config.settings or {}, {
-                                    Lua = {
-                                        workspace = {
-                                            library = {
-                                                [vim.fn.expand("C:\\Program Files (x86)\\Steam\\steamapps\\common\\Factorio\\doc-html\\node_modules\\.bin")] = true,
-                                            },
-                                        },
+
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "lua_ls",
+                    "clangd",
+                    "pyright",
+                },
+                handlers = {
+                    -- Default handler for all servers
+                    function(server_name)
+                        require("lspconfig")[server_name].setup {}
+                    end,
+
+                    -- Custom handler for lua_ls
+                    ["lua_ls"] = function()
+                        local lspconfig = require("lspconfig")
+                        local opts = {
+                            settings = {
+                                Lua = {
+                                    workspace = {
+                                        library = {},
                                     },
-                                })
-                            end
-                        end
-                    end
-                end
+                                },
+                            },
+                                -- Only apply Factorio-specific config on Windows
+                            on_new_config = function(new_config, new_root_dir)
+                                if vim.loop.os_uname().sysname == "Windows_NT" then
+                                    if new_root_dir:find("Factorio") then
+                                        new_config.settings.Lua.workspace.library[vim.fn.expand("C:\\Program Files (x86)\\Steam\\steamapps\\common\\Factorio\\doc-html\\node_modules\\.bin")] = true
+                                    end
+                                end
+                            end,
+                        }
 
-                lspconfig[server].setup(opts)
-            end
-        end,
+                        lspconfig.lua_ls.setup(opts)
+                    end,
+                },
+            })
+        end
     },
 
     {
