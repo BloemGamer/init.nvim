c5cb96dda868ea0f97f5a478799c64c58aaa6623
Bloem <semhammann@gmail.com>
2025-04-28 21:06:54 +0200

    added new build ignore

--- lua/bloem/plugins/telescope.lua
+++ lua/bloem/plugins/telescope.lua
@@ -21,7 +21,7 @@ return
                     "%.idea/*",
                     "%.venv/*",
                     "__pycache__/*",
-                    "%builddir",
+                    "%build*",
                 },
             },
         })
