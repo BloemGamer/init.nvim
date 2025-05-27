4b3fd025210c75b171c55ad21ec6c317b6840c11
Bloem <sem@ie-design.nl>
2025-05-27 11:08:58 +0200

    removed the error if diff is not found

--- lua/bloem/plugins/undotree.lua
+++ lua/bloem/plugins/undotree.lua
@@ -3,5 +3,8 @@ return
     'mbbill/undotree',
     config = function()
         vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
+        if vim.fn.executable("diff") == 0 then
+            vim.g.undotree_DiffAutoOpen = 0
+        end
     end
 }
