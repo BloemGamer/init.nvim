899c17b9da5acfb69344c1b2561aba090d4f316c
Bloem <sem@ie-design.nl>
2025-09-14 23:04:04 +0200

    removed neovide, because I do not use it anymore

--- init.lua
+++ init.lua
@@ -1,14 +1,1 @@
 require("bloem")
-
-if vim.g.neovide then
-    -- GUI font with no ligatures
-    vim.o.guifont = "Cascadia Mono:h14"
-
-    -- Disable all cursor animations
-    vim.g.neovide_cursor_animation_length = 0
-    vim.g.neovide_cursor_trail_length = 0
-    vim.g.neovide_cursor_vfx_mode = ""
-
-    -- Start in fullscreen mode
-    vim.g.neovide_fullscreen = true
-end
