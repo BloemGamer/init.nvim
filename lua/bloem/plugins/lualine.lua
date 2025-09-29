73d15e614627440df5fd32d56f7a7fc846f1d0c9
Bloem <sem@ie-design.nl>
2025-09-29 12:27:53 +0200

    added lualine lsp_progress

--- lua/bloem/plugins/lualine.lua
+++ lua/bloem/plugins/lualine.lua
@@ -1,26 +1,32 @@
 return {
-	'nvim-lualine/lualine.nvim',
-	dependencies = { 'nvim-tree/nvim-web-devicons' },
-	config = function ()
-		require('lualine').setup {
-			options = {
-				icons_enabled = false,  -- disable all icons
-				theme = "catppuccin",
-			},
-			sections = {
-				lualine_a = { "mode" },
-				lualine_b = { "branch", "diff", "diagnostics" },
-				lualine_c = {
-					{ "filename" },
-					{
-						function() return require("nvim-navic").get_location() end,
-						cond = function() return require("nvim-navic").is_available() end,
-					},
+	{
+		'nvim-lualine/lualine.nvim',
+		dependencies = {
+			'nvim-tree/nvim-web-devicons',
+			'arkav/lualine-lsp-progress',
+		},
+		config = function ()
+			require('lualine').setup {
+				options = {
+					icons_enabled = true,
+					theme = "catppuccin",
 				},
-				lualine_x = { "encoding", "fileformat", "filetype" },
-				lualine_y = { "progress" },
-				lualine_z = { "location" },
-			},
-		}
-	end
+				sections = {
+					lualine_a = { "mode" },
+					lualine_b = { "branch", "diff", "diagnostics" },
+					lualine_c = {
+						"filename",
+						"lsp_progress",
+						{
+							function() return require("nvim-navic").get_location() end,
+							cond = function() return require("nvim-navic").is_available() end,
+						},
+					},
+					lualine_x = { "encoding", "fileformat", "filetype" },
+					lualine_y = { "progress" },
+					lualine_z = { "location" },
+				},
+			}
+		end
+	},
 }
