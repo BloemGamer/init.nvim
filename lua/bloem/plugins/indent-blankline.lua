9fd2259d4465b610917a30c4d5bd45259991eec3
Bloem <sem@ie-design.nl>
2025-10-05 22:41:06 +0200

    added that you can see whitespace now

--- lua/bloem/plugins/indent-blankline.lua
+++ lua/bloem/plugins/indent-blankline.lua
@@ -1,10 +1,26 @@
 return {
-    "lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
-    ---@module "ibl"
-    ---@type ibl.config
-    opts = {},
+	"lukas-reineke/indent-blankline.nvim",
+	main = "ibl",
+	---@module "ibl"
+	---@type ibl.config
+	opts = {},
 	config = function ()
-		require("ibl").setup()
+		require("ibl").setup {
+			scope = { enabled = false },
+			indent = {
+				char = "│",
+				tab_char = "│",
+			},
+		}
+
+		vim.opt.list = true
+		vim.opt.listchars = {
+			space = '·',
+			tab = '→ ',      -- Arrow followed by space
+			trail = '•',
+			extends = '⟩',
+			precedes = '⟨',
+			nbsp = '␣',
+		}
 	end
 }
