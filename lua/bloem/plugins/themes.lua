8f3220c1bb4003d396e5930e9d38c1e4ceaae869
Bloem <sem@ie-design.nl>
2025-10-06 10:50:45 +0200

    removed not used colourscemes

--- lua/bloem/plugins/themes.lua
+++ lua/bloem/plugins/themes.lua
@@ -10,26 +10,6 @@ function ColourPencils(color)
 end
 
 return {
-    {
-        "rose-pine/neovim",
-        name = "rose-pine",
-        config = function()
-			require('rose-pine').setup({
-				-- disable_background = true,
-				styles = {
-					italic = true,
-				},
-			})
-			ColourPencils()
-        end
-    },
-    {
-        "Mofiqul/vscode.nvim",
-        as = 'vscode',
-        config = function()
-            ColourPencils()
-        end
-    },
 	{
 		"catppuccin/nvim",
 		name = "catppuccin",
