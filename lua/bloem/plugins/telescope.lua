6a8ac7a9af05f2764d8e53e3efcecff9c228ef7f
Bloem <sem@ie-design.nl>
2025-10-14 10:42:01 +0200

    added a keybind, now shows the git ignored files, and hidden sdkconfig

--- lua/bloem/plugins/telescope.lua
+++ lua/bloem/plugins/telescope.lua
@@ -15,28 +15,29 @@ return {
 			defaults = {
 				-- Ignore .git directories when searching for files
 				file_ignore_patterns = {
-					"%.exe",
-					"%.git/*",
-					"%.vscode/*",
-					"%.idea/*",
-					"%.venv/*",
-					"__pycache__/*",
-					"%build*",
-					"%.cache*",
-					"%addons*",
-					"*.svg",
-					".uid",
-					".tscn",
-					".import",
-					".svg",
-					".godot",
-					"target",
+					"%.exe$",
+					"%.git/",
+					"%.vscode/",
+					"%.idea/",
+					"%.venv/",
+					"__pycache__/",
+					"build/",
+					"%.cache/",
+					"addons/",
+					"%.svg$",
+					"%.uid$",
+					"%.tscn$",
+					"%.import$",
+					"%.godot$",
+					"target/",
 					"Cargo.lock",
-				},
+					"sdkconfig*"
+				}
 			},
 		})
 
-		vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
+		vim.keymap.set('n', '<leader>pf', function() builtin.find_files({ no_ignore = true }) end, { desc = 'Telescope find files' })
+		vim.keymap.set('n', '<leader>ph', function() builtin.find_files({ no_ignore = true, hidden = true }) end, { desc = 'Telescope find files' })
 		vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope find files git' })
 		vim.keymap.set('n', '<leader>pg', builtin.live_grep, { desc = 'Telescope find with grep' })
 		vim.keymap.set('n', '<leader>ps', builtin.lsp_document_symbols, { desc = 'Telescope find with grep' })
