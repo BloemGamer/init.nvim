eedeff00172e21e7c4cffd514d3ef57d4d53ae77
Bloem <sem@ie-design.nl>
2025-10-28 10:43:12 +0100

    made the 'J' and 'K' silent so it works a lot faster

--- lua/bloem/remap.lua
+++ lua/bloem/remap.lua
@@ -8,8 +8,8 @@ vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', 'n', 'nzz')
 vim.keymap.set('n', 'N', 'Nzz')
 
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
-vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", {silent = true})
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", {silent = true})
 
 vim.keymap.set('x', '<leader>p', '\"_dP')
 
