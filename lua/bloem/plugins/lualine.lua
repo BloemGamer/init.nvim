000b488ab9ffa9e122ad90ef1832ce7df2d958e8
Bloem <sem@ie-design.nl>
2025-11-05 21:51:45 +0100

    added recording to lualine

--- lua/bloem/plugins/lualine.lua
+++ lua/bloem/plugins/lualine.lua
@@ -1,12 +1,21 @@
 return {
 	{
-		'nvim-lualine/lualine.nvim',
+		"nvim-lualine/lualine.nvim",
 		dependencies = {
-			'nvim-tree/nvim-web-devicons',
-			'catppuccin/nvim',
+			"nvim-tree/nvim-web-devicons",
+			"catppuccin/nvim",
 		},
-		config = function ()
-			require('lualine').setup {
+		config = function()
+			local function recording_status()
+				local reg = vim.fn.reg_recording()
+				if reg == "" then
+					return ""
+				else
+					return "@" .. reg
+				end
+			end
+
+			require("lualine").setup({
 				options = {
 					icons_enabled = true,
 					theme = "catppuccin",
@@ -17,15 +26,30 @@ return {
 					lualine_c = {
 						"filename",
 						{
-							function() return require("nvim-navic").get_location() end,
-							cond = function() return require("nvim-navic").is_available() end,
+							function()
+								return require("nvim-navic").get_location()
+							end,
+							cond = function()
+								return require("nvim-navic").is_available()
+							end,
 						},
 					},
-					lualine_x = { "encoding", "fileformat", "filetype" },
+					lualine_x = {
+						recording_status,
+						"encoding",
+						"fileformat",
+						"filetype",
+					},
 					lualine_y = { "progress" },
 					lualine_z = { "location" },
 				},
-			}
-		end
+			})
+
+			vim.api.nvim_create_autocmd({ "RecordingEnter", "RecordingLeave" }, {
+				callback = function()
+					vim.defer_fn(require("lualine").refresh, 50)
+				end,
+			})
+		end,
 	},
 }
