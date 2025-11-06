4337ab3e2ffa15571a6cfd5a83da5f14ed88f899
Bloem <sem@ie-design.nl>
2025-11-06 22:32:27 +0100

    added some usefull autocmd's

--- lua/bloem/set.lua
+++ lua/bloem/set.lua
@@ -24,6 +24,8 @@ vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
+vim.opt_local.cursorline = true
+
 --vim.api.nvim_set_option("clipboard", "unnamedplus")
 
 
@@ -45,3 +47,59 @@ vim.cmd [[
 	highlight ExtraWhitespace ctermbg=red guibg=red
 	autocmd BufWinEnter * match ExtraWhitespace /\s\+$/
 ]]
+
+vim.api.nvim_create_autocmd("textyankpost", {
+	group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
+	pattern = "*",
+	desc = "highlight selection on yank",
+	callback = function ()
+		vim.highlight.on_yank({ timeout = 200, visual = true })
+	end
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "help",
+	command = "wincmd L",
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+	group = vim.api.nvim_create_augroup("no_auto_comment", { }),
+	desc = "no auto comment when going to next line",
+	callback = function ()
+		vim.opt_local.formatoptions:remove({ "c", "r", "o" })
+	end
+})
+
+-- ide like highlight when stopping cursor
+vim.api.nvim_create_autocmd("CursorMoved", {
+	group = vim.api.nvim_create_augroup("LspReferenceHighlight", { clear = true }),
+	desc = "Highlight references under cursor",
+	callback = function()
+		-- Only run if the cursor is not in insert mode
+		if vim.fn.mode() ~= "i" then
+			local clients = vim.lsp.get_clients({ bufnr = 0 })
+			local supports_highlight = false
+			for _, client in ipairs(clients) do
+				if client.server_capabilities.documentHighlightProvider then
+					supports_highlight = true
+					break -- Found a supporting client, no need to check others
+				end
+			end
+
+			-- 3. Proceed only if an LSP is active AND supports the feature
+			if supports_highlight then
+				vim.lsp.buf.clear_references()
+				vim.lsp.buf.document_highlight()
+			end
+		end
+	end,
+})
+
+-- ide like highlight when stopping cursor
+vim.api.nvim_create_autocmd("CursorMovedI", {
+	group = "LspReferenceHighlight",
+	desc = "Clear highlights when entering insert mode",
+	callback = function()
+		vim.lsp.buf.clear_references()
+	end,
+})
