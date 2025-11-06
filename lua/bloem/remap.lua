8754c7c0032e3615fd888375aa854fc4d2127306
Bloem <sem@ie-design.nl>
2025-11-06 11:06:30 +0100

    removed the Q remap to nothing

--- lua/bloem/remap.lua
+++ lua/bloem/remap.lua
@@ -4,7 +4,7 @@ vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 
 
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
-vim.keymap.set('n', '<C-d>', '<C-d>zz') 
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', 'n', 'nzz')
 vim.keymap.set('n', 'N', 'Nzz')
 
@@ -12,8 +12,6 @@ vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", {silent = true})
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", {silent = true})
 
 vim.keymap.set('x', '<leader>p', '\"_dP')
-
-vim.keymap.set("n", "Q", "<nop>")
 
 vim.keymap.set('n', '<Up>',    '<C-w>k', { noremap = true, silent = true })
 vim.keymap.set('n', '<Down>',  '<C-w>j', { noremap = true, silent = true })
