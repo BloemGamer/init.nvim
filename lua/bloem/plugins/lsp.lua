6d8bf98cd778726dd6e8850b2fcd0d4ae3d00e35
Bloem <sem@ie-design.nl>
2025-11-25 23:22:10 +0100

    fixed an error where tab after entering insert mode did weird

--- lua/bloem/plugins/lsp.lua
+++ lua/bloem/plugins/lsp.lua
@@ -340,6 +340,19 @@ return {
 					{ name = 'cmdline' }
 				})
 			})
+			local function leave_snippet()
+				if ((vim.v.event.old_mode == 's' and vim.v.event.new_mode == 'n') or vim.v.event.old_mode == 'i')
+					and require('luasnip').session.current_nodes[vim.api.nvim_get_current_buf()]
+					and not require('luasnip').session.jump_active
+					then
+						require('luasnip').unlink_current()
+					end
+				end
+
+				vim.api.nvim_create_autocmd("ModeChanged", {
+					pattern = "*",
+					callback = leave_snippet
+				})
 		end,
 	},
 
