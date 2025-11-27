8de89a34c48f5478f4251f47ad9ff6c8d5121d82
Bloem <sem@ie-design.nl>
2025-11-27 11:27:20 +0100

    removed the background

--- lua/bloem/plugins/themes.lua
+++ lua/bloem/plugins/themes.lua
@@ -2,11 +2,34 @@ function ColourPencils(color)
 	color = color or "catppuccin"
 	vim.cmd.colorscheme(color)
 
-	-- vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
-	-- vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
+	RemoveBackground()
 
 	vim.cmd("highlight Comment gui=italic")
 	vim.cmd("highlight Keyword gui=bold")
+end
+
+function RemoveBackground()
+	vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
+	vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
+	-- Remove Telescope background
+	vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "none" })
+	vim.api.nvim_set_hl(0, "TelescopePromptNormal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "TelescopePromptBorder", { bg = "none" })
+	vim.api.nvim_set_hl(0, "TelescopePromptTitle", { bg = "none" })
+	vim.api.nvim_set_hl(0, "TelescopePreviewTitle", { bg = "none" })
+	vim.api.nvim_set_hl(0, "TelescopeResultsTitle", { bg = "none" })
+
+	-- Clear window separator backgrounds
+	vim.api.nvim_set_hl(0, "WinSeparator", { bg = "none" })
+	vim.api.nvim_set_hl(0, "VertSplit", { bg = "none" })
+
+	-- Clear other window-related backgrounds
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" }) -- Non-current windows
+	vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+	vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" }) -- ~ characters
+
 end
 
 return {
@@ -33,6 +56,14 @@ return {
 				},
 			}
 			ColourPencils()
+			vim.api.nvim_create_autocmd("ColorScheme", {
+				pattern = "*",
+				callback = function()
+					-- Clear backgrounds here
+					vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
+					-- ... rest of the highlights
+				end,
+			})
 		end
 	}
 }
