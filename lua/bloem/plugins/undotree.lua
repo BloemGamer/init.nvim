a799166e39a1ae850648c130b7af03cb45821268
Bloem <sem@ie-design.nl>
2026-01-31 12:23:37 +0100

    added undotree

---
+++ lua/bloem/plugins/undotree.lua
@@ -0,0 +1,9 @@
+return {
+    'mbbill/undotree',
+    config = function()
+        vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
+        if vim.fn.executable("diff") == 0 then
+            vim.g.undotree_DiffAutoOpen = 0
+        end
+    end
+}
